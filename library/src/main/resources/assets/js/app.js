e22ed21b7eb4891c1e33917f7893a597e30095a3
preetiborade <preeti.borade@gmail.com>
2014-04-12 21:11:53 -0700

    reporting status of a book

--- library/src/main/resources/assets/js/app.js
+++ library/src/main/resources/assets/js/app.js
@@ -1,4 +1,18 @@
 $(":button").click(function() {
 	var isbn = this.id;
 	alert('About to report lost on ISBN ' + isbn);
+	var url = "/library/v1/books/"+ isbn +"?status=lost";
+
+	var ajax = $.ajax({
+	    url: url,
+	    type: "PUT",
+	    dataType: "json",
+	    contentType:"application/json",
+	    success: function(response) {
+	        $('#'+isbn+'bookStatus').html("lost");
+	        $('#'+isbn).prop("disabled",true);
+	    }
+
+	});
+	alert( 'Book with ISBN ' + isbn + ' has been reported lost.');
 });
